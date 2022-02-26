8905a4bb0cab4787e385d327942c042d366c39a0
Elvis-codeur <83865024+Elvis-codeur@users.noreply.github.com>
2022-02-26 11:05:13 +0000

    First commit

---
+++ data_fetching_test/data_fetching_test.ino
@@ -0,0 +1,13 @@
+void setup() {
+  int lecture_pin = 8;
+  pinMode(lecture_pin,INPUT);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  int valeur_lue = analogRead(lecture_pin);
+
+}
